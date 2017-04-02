82a175eecd63cd46d46573afd6b8c1731f6f4b8c
HOME\kailinOdonnell <kailin_5@hotmail.com>
2017-04-02 09:42:45 +0100

    no message

--- NemApi/Wrapper/Model/DataModels/AggregateModificationData.cs
+++ NemApi/Wrapper/Model/DataModels/AggregateModificationData.cs
@@ -4,6 +4,29 @@
 
 namespace CSharp2nem
 {
+    /*
+     * The data required to modify a multisignature account
+     * Must be initiated by a cosignatory.
+     * 
+     * @Modifications { AggregateModification } The modifications to make to the account 
+     *      {
+     *          ModificationType 1 for add, 2 for remove
+     *          PublicKey The account to remove or add as cosignatory
+     *      }
+     * @MultisigAccount { PublicKey } The multisig account to be modified
+     * @RelativeChange { int } The change to the number of minimum cosignatories
+     *                         ie. If a multisig account is 2 of 3, a relative change 
+     *                         of -1 (without a corrosponding Aggregate modification)
+     *                         attached, this will result in a 1 of 3 multisig account. 
+     *                         If a cosignatory is then added so that there are 4 signatory
+     *                         accounts, to make the account 3 of 4, a relative change of 2 
+     *                         should be included, otherwise the multisig account will remain
+     *                         as 1 of 4. An addition of a cosignatory account does not increase
+     *                         the minimum cosignatories required for a transaction.
+     * 
+     * @Deadline { int } The deadline when the transaction must be accepted before it is cancled
+     * 
+     */
     public class AggregateModificationData
     {
         public List<AggregateModification> Modifications { get; set; }
--- NemApi/Wrapper/Model/DataModels/ImportanceTransferData.cs
+++ NemApi/Wrapper/Model/DataModels/ImportanceTransferData.cs
@@ -4,6 +4,19 @@ namespace CSharp2nem
 {
     public class ImportanceTransferData
     {
+        /*
+         * Importance transfer data
+         * 
+         * @MultisigAccount { PublicKey } The multisig account that should transfer its importance
+         *                                Note: use this when initiating from a cosignatory. 
+         *                                When left as null, the transaction is initiated for 
+         *                                the signing account
+         * 
+         * @DelegatedAccount { PublicKey } The account to transfer importance to
+         * @Activate { bool } Set true to activate an importance transfer, false to revoke an importance transfer
+         * @Deadline { int } The deadline when the transction must be accepted by.
+         * 
+         */
         public PublicKey MultisigAccount { get; set; }
         public PublicKey DelegatedAccount { get; set; }
         public bool Activate { get; set; }
